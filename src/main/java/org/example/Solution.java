cabcc9a91368f77ca32fdbb90eeaccd6ff8badce
Rohit Mishra <rrohitt33@gmail.com>
2025-01-21 23:28:49 +0530

    adding sample leetcode solution

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,61 +4,38 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 class Solution {
-    int[] firstIndexes=new int[26];
-    int[] lastIndexes=new int[26];
-    public int countPalindromicSubsequence(String s) {
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(firstIndexes[s.charAt(i)-'a']==0){
-                firstIndexes[s.charAt(i)-'a']=i+1;
-            }
-            lastIndexes[s.charAt(i)-'a']=i+1;
-        }
-        ArrayList<HashSet<Character>> al=new ArrayList<>(26);
-        for(int i=0;i<26;i++){
-            al.add(new HashSet<>());
-        }
-        // for(int i=0;i<26;i++){
-        //     System.out.println("firstIndex: "+firstIndexes[i]+" lastIndex: "+lastIndexes[i]);
-        // }
-        for(int i=0;i<n;i++){
-            char c=s.charAt(i);
-            int value=i+1;
-            for(int j=0;j<26;j++){
-                if(firstIndexes[j]<value&&lastIndexes[j]>value){
-                    al.get(j).add(c);
-                }
-            }
-        }
-        int sum=0;
-        int i=0;
-        for(HashSet<Character> hs:al){
-            // System.out.println(i+" "+hs.size());
-            sum+=hs.size();
-        }
-        return sum;
+
+    public static void main(String[] args) {
+        Solution sl=new Solution();
+        // [[3,3,1],[8,5,2]]
+        int[][] grid={{20,3,20,17,2,12,15,17,4,15},{20,10,13,14,15,5,2,3,14,3}};
+        long res=sl.gridGame(grid);
+        System.out.println(res);
     }
 
-    public String shiftingLetters(String s, int[][] shifts) {
-        int[] a=new int[s.length()];
-        for(int[] query:shifts){
-            int valueAdded=query[2]==0?-1:1;
-            a[query[0]]+=valueAdded;
-            if(query[1]<a.length-1){
-                a[query[1]+1]-=valueAdded;
+    public long gridGame(int[][] grid) {
+        long firstRowSum=0l;
+        for(int i:grid[0]){
+            firstRowSum+=grid[0][i];
+        }
+        long sum1=0l;
+        long sum2=0l;
+        long res=Long.MAX_VALUE;
+        for(int i=0;i<grid[0].length;i++){
+            long min=0;
+            sum1+=grid[0][i];
+            if(i==0){
+                min=firstRowSum-(sum1);
             }
-        }
-        for(int i=1;i<a.length;i++){
-            a[i]+=a[i-1];
-        }
-        char[] resArray=new char[a.length];
-        for(int i=0;i<s.length();i++){
-            int value=a[i]%26;
-            if(value<0){
-                value=26+value;
+            else if(i==grid[0].length-1){
+                min=sum2;
             }
-            resArray[i]=(char)('a'+(s.charAt(i)-'a'+value)%26);
+            else{
+                min=Math.max(firstRowSum-sum1,sum2);
+            }
+            res=Math.min(min,res);
+            sum2+=grid[1][i];
         }
-        return new String(resArray);
+        return res;
     }
 }
